5b7466be90076e9e3e05fd088152c47f4db7f20b
kasraouiii <kasraoui.mouad@gmail.com>
2021-06-30 15:18:12 +0100

    Add files via upload

---
+++ EXTSGMAFOT-103 update .sql
@@ -0,0 +1,20 @@
+
+----103
+
+Insert into LKCKEGRE (CKEID,GRECODE,EGRFLAGHIDDEN,EGRFLAGMANDATORY,EGRFLAGDISABLED,EGRFLAGCOLLAPSED,EGRMASKCODE) 
+values ('1884','frmActeurDetail.acteurDetailBody.pnlGrp.rootSectionContainer.secRelatedActors.secRelatedActorsAsc.secRelatedActorsAscMaster.tblRelatAsc.btnDelete','1','0','0','0',null);
+
+Insert into LKCKEGRE (CKEID,GRECODE,EGRFLAGHIDDEN,EGRFLAGMANDATORY,EGRFLAGDISABLED,EGRFLAGCOLLAPSED,EGRMASKCODE) 
+values ('1884','frmActeurDetail.acteurDetailBody.pnlGrp.rootSectionContainer.secRole.secListRole.pnlGrpFrmActRole.secActRoleDetail.tblAroAge.btnNew','1','0','0','0',null);
+
+
+Insert into LKCKEGRE (CKEID,GRECODE,EGRFLAGHIDDEN,EGRFLAGMANDATORY,EGRFLAGDISABLED,EGRFLAGCOLLAPSED,EGRMASKCODE) 
+values ('1884','frmActeurDetail.acteurDetailBody.pnlGrp.rootSectionContainer.secRelatedActors.secDescending.pnlGrpHierarchieDescendante.tblRelDsc.btnDelete','1','0','0','0',null);
+
+
+Insert into LKCKEGRE (CKEID,GRECODE,EGRFLAGHIDDEN,EGRFLAGMANDATORY,EGRFLAGDISABLED,EGRFLAGCOLLAPSED,EGRMASKCODE) 
+values ('1884','frmActeurDetail.acteurDetailBody.pnlGrp.rootSectionContainer.secRole.secListRole.pnlGrpFrmActRole.secActRoleDetail.tblAroAge.btnDelete','1','0','0','0',null);
+
+
+
+commit ;
