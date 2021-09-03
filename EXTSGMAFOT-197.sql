6eb51a7026ca2ac4e19fde59f1ef023815737b51
kasraouiii <kasraoui.mouad@gmail.com>
2021-09-03 18:14:36 +0100

    Add files via upload

---
+++ EXTSGMAFOT-197.sql
@@ -0,0 +1,10 @@
+
+
+-----EXTSGMAFOT-197
+
+delete  from relationvaleurprofil  where reccode='USERDECI' and revfillecode='DECIFA01'  and tpgcode = 'AUT02' and revmerecode ='RISQ' ;
+delete  from relationvaleurprofil  where reccode='USERGRPDECI' and revfillecode='DECIFA01'  and tpgcode = 'AUT02' and revmerecode ='RISQUE' ;
+
+
+
+commit ;
