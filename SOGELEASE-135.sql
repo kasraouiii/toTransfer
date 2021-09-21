55cc38a95f61dcb7a917c3d8dc123e674dbb6450
kasraouiii <kasraoui.mouad@gmail.com>
2021-09-21 15:40:09 +0100

    Add files via upload

---
+++ SOGELEASE-135.sql
@@ -0,0 +1,43 @@
+Insert into TTRPARAM
+   (TTRNOM, TTPCODE)
+ Values
+   ('TYPEIDENTIFIANT', 'CIN');
+   
+ Insert into TTRPARAM
+   (TTRNOM, TTPCODE)
+ Values
+   ('TYPEIDENTIFIANT', 'SEJ');
+   
+ Insert into TTRPARAM
+   (TTRNOM, TTPCODE)
+ Values
+   ('TYPEIDENTIFIANT', 'DIP');    
+   
+   
+  Insert into TTRPARAM
+   (TTRNOM, TTPCODE)
+ Values
+   ('TYPEIDENTIFIANT', 'PAS');     
+   
+
+
+Insert into LANTTRPARAM
+   (TTRNOM, TTPCODE, LANCODE, TTPLIBELLE)
+ Values
+   ('TYPEIDENTIFIANT', 'CIN', 'FR', 'CIN');
+   
+ Insert into LANTTRPARAM
+   (TTRNOM, TTPCODE, LANCODE, TTPLIBELLE)
+ Values
+   ('TYPEIDENTIFIANT', 'SEJ', 'FR', 'Carte de séjour');  
+   
+  Insert into LANTTRPARAM
+   (TTRNOM, TTPCODE, LANCODE, TTPLIBELLE)
+ Values
+   ('TYPEIDENTIFIANT', 'DIP', 'FR', 'Carte diplomatique');   
+   
+   
+    Insert into LANTTRPARAM
+   (TTRNOM, TTPCODE, LANCODE, TTPLIBELLE)
+ Values
+   ('TYPEIDENTIFIANT', 'PAS', 'FR', 'Passeport'); 
