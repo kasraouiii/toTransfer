9f48d62dbc05ccc85d14b9927b24379acf6fde08
kasraouiii <kasraoui.mouad@gmail.com>
2021-10-25 15:52:45 +0100

    Add files via upload

---
+++ EXTSGMAFOT-245.sql
@@ -0,0 +1,9 @@
+
+---EXTSGMAFOT-245
+
+
+update lkckegre   set egrflagdisabled =0  where grecode='dealDetail.dealDetailBodySid.tabPane.secDocumentManagement.secDocManagement.secDocManagementMaster.tblDocManagement.btnDelete'
+ and ckeid = 2005     and  egrflagdisabled =1   ;
+ 
+ 
+ commit ;
