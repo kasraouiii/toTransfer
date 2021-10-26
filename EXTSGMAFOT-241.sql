fd312585e3d465641d41e227201110448dacd05f
kasraouiii <kasraoui.mouad@gmail.com>
2021-10-26 09:06:41 +0100

    Add files via upload

---
+++ EXTSGMAFOT-241.sql
@@ -0,0 +1,9 @@
+-------EXTSGMAFOT-241
+
+
+Insert into LKCKEGRE (CKEID,GRECODE,EGRFLAGHIDDEN,EGRFLAGMANDATORY,EGRFLAGDISABLED,EGRFLAGCOLLAPSED,EGRMASKCODE) 
+values ('10','EVF_TIRAGE.dlgEvEvfTirageMainSid.pnlEvfTirage.secEnvSituation.pnlenvsituation.tfdDateLimiteEnv','0','0','1','0',null);
+
+
+
+commit ;
