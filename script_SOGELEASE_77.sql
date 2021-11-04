5f5f3ce8be984623287e749f3ced60137770c616
kasraouiii <kasraoui.mouad@gmail.com>
2021-11-04 12:50:46 +0100

    Add files via upload

---
+++ script_SOGELEASE_77.sql
@@ -0,0 +1,53 @@
+/********************/
+
+UPDATE GRTCOLUMN 
+set GCOCODE = 'Actein' 
+where GRTCODE = 'SELECT_ACTOR' 
+and  GCOCODE = 'NumCIN';
+
+UPDATE LANGRTCOLUMN 
+set GCOCODE = 'Actein' 
+where GRTCODE = 'SELECT_ACTOR' 
+and  GCOCODE = 'NumCIN';
+---------
+
+UPDATE GRTCOLUMN 
+set GCOCODE = 'Actcardmanagement' 
+where GRTCODE = 'SELECT_ACTOR' 
+and  GCOCODE = 'NumCartDip';
+
+UPDATE LANGRTCOLUMN 
+set GCOCODE = 'Actcardmanagement' 
+where GRTCODE = 'SELECT_ACTOR' 
+and  GCOCODE = 'NumCartDip';
+-----------
+UPDATE GRTCOLUMN 
+set GCOCODE = 'Actcardtransaction' 
+where GRTCODE = 'SELECT_ACTOR' 
+and  GCOCODE = 'NumCartSej';
+
+UPDATE LANGRTCOLUMN 
+set GCOCODE = 'Actcardtransaction' 
+where GRTCODE = 'SELECT_ACTOR' 
+and  GCOCODE = 'NumCartSej';
+------------
+UPDATE GRTCOLUMN 
+set GCOCODE = 'Numice' 
+where GRTCODE = 'SELECT_ACTOR' 
+and  GCOCODE = 'NumICE';
+UPDATE LANGRTCOLUMN 
+set GCOCODE = 'Numice' 
+where GRTCODE = 'SELECT_ACTOR' 
+and  GCOCODE = 'NumICE';
+------------------
+UPDATE GRTCOLUMN 
+set GCOCODE = 'Idfiscal' 
+where GRTCODE = 'SELECT_ACTOR' 
+and  GCOCODE = 'IdFiscal';
+
+UPDATE LANGRTCOLUMN 
+set GCOCODE = 'Idfiscal' 
+where GRTCODE = 'SELECT_ACTOR' 
+and  GCOCODE = 'IdFiscal';
+
+/**************************/
