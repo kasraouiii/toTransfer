0f3c17eddff23b15b4b9ab071ba7d4a88ebfef9b
kasraouiii <kasraoui.mouad@gmail.com>
2021-11-04 10:56:19 +0100

    Add files via upload

---
+++ EXTSGMAFOT-273.sql
@@ -0,0 +1,8 @@
+
+----EXTSGMAFOT-273
+
+delete from lantusparam  where tusnom='CLACULLAD' and tupcode in ( '0', '1') and lancode='FR'  ;
+delete from tusparam  where tusnom='CLACULLAD' and tupcode in ( '0', '1')   ;
+
+
+commit ; 
