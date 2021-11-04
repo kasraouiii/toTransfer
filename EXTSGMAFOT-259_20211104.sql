4ad6749eb6aa920a6672f96624dfb4d2f0d51fdd
kasraouiii <kasraoui.mouad@gmail.com>
2021-11-04 17:30:10 +0100

    Add files via upload

---
+++ EXTSGMAFOT-259_20211104.sql
@@ -0,0 +1,10 @@
+
+
+-----EXTSGMAFOT-259
+
+Insert into LKCKEGRE (CKEID,GRECODE,EGRFLAGHIDDEN,EGRFLAGMANDATORY,EGRFLAGDISABLED,EGRFLAGCOLLAPSED,EGRMASKCODE) 
+values ('10','dealDetail.dealDetailBodySid.tabPane.secDealActeur.pngIdentify.pnlIdentify.tfdActcodeclient','0','1','0','0',null);
+
+
+
+commit ;
