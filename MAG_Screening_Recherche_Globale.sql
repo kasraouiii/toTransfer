c33ff3fb24d74397522b83b37075c9d059cb365d
kasraouiii <kasraoui.mouad@gmail.com>
2021-11-26 12:07:45 +0100

    Add files via upload

---
+++ MAG_Screening_Recherche_Globale.sql
@@ -0,0 +1,21 @@
+
+delete from lanENTITY where ENTCODE='DEPENSE' and LANCODE='FR';
+delete  from SCREEN where ENTCODE='DEPENSE';
+delete  from ENTITY where ENTCODE='DEPENSE';
+
+delete from lanENTITY where ENTCODE='DOSSIER' and LANCODE='FR';
+delete  from SCREEN where ENTCODE='DOSSIER';
+delete  from ENTITY where ENTCODE='DOSSIER';
+
+delete from lanENTITY where ENTCODE='FACTURE' and LANCODE='FR';
+delete  from SCREEN where ENTCODE='FACTURE';
+delete  from ENTITY where ENTCODE='FACTURE';
+
+delete from lanENTITY where ENTCODE='IMMOTRANCHE' and LANCODE='FR';
+delete  from SCREEN where ENTCODE='IMMOTRANCHE';
+delete  from ENTITY where ENTCODE='IMMOTRANCHE';
+
+delete from lanENTITY where ENTCODE='REGLEMENT' and LANCODE='FR';
+delete  from SCREEN where ENTCODE='REGLEMENT';
+delete  from ENTITY where ENTCODE='REGLEMENT';
+
